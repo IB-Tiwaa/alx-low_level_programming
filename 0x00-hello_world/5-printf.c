daf779bb9a828c7d77357d4280e12e9098689e17
IB-Tiwaa <bellasuccess18@gmail.com>
2023-09-14 12:19:09 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - is my function
+ * Return: 0 is a success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
