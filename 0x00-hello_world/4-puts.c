13f2c1068406ca40de3dc71aeee619a1c143e2df
IB-Tiwaa <bellasuccess18@gmail.com>
2023-09-14 12:00:28 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - is my function
+ * Return: 0 is a success
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle \n");
+
+	return (0);
+}
