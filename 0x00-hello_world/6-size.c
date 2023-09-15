cee956a8c2f9fa9e010b3d4c64e85966cffdba10
IB-Tiwaa <bellasuccess18@gmail.com>
2023-09-15 03:26:46 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	int Float = sizeof(float);
 
 	printf("Size of a char: %d byte(s)\n", Character);
-	printf("Size of a int: %d byte(s)\n", Integer);
+	printf("Size of an int: %d byte(s)\n", Integer);
 	printf("Size of a long int: %d byte(s)\n", LInteger);
 	printf("Size of a long long int: %d byte(s)\n", long_long_int);
 	printf("Size of a float: %d byte(s)\n", Float);
