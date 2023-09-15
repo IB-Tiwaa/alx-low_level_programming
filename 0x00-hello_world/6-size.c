68d720eac966d4406f06be32f44426211a9671b9
IB-Tiwaa <bellasuccess18@gmail.com>
2023-09-15 02:58:49 -0700

    size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - is my function
+ * Return: 0 is a success
+ */
+int main(void)
+{
+	int Integer = sizeof(int);
+	int Character = sizeof(char);
+	int LInteger = sizeof(long int);
+	int LlInteger = sizeof(long long int);
+	int Float = sizeof(float);
+
+	printf("Size of a char: %d byte(s)\n", Integer);
+	printf("Size of a char: %d byte(s)\n", Character);
+	printf("Size of a char: %d byte(s)\n", LInteger);
+	printf("Size of a char: %d byte(s)\n", LlInteger);
+	printf("Size of a char: %d byte(s)\n", Float);
+
+	return (0);
+}
